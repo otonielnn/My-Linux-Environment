75fc69be4781255a2c3ce515d77715faab5ed6b5
otonielnn <otoniel_2002@live.com>
2024-07-11 18:07:34 -0300

    fixing script
    Signed-off-by: otonielnn <otoniel_2002@live.com>

--- script.sh
+++ script.sh
@@ -17,7 +17,7 @@ echo -e "\e[34;1mBaixando e Instalando NVM e Node...\e[0m"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-bash && nvm install --lts
+nvm install --lts
 
 echo -e "\e[34;1mBaixando e Instalando docker...\e[0m"
 sudo apt install docker.io docker-compose -y
