b3833a7416a2d90eb47b4112a4e11748c563e9a4
otonielnn <otoniel_2002@live.com>
2024-07-12 18:04:57 -0300

    add validation to git and curl
    Signed-off-by: otonielnn <otoniel_2002@live.com>

--- script.sh
+++ script.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-echo -e "\e[34;1mAtualizando o Sistema...\e[0m"
-sudo apt update
-sudo apt upgrade -y
-sudo apt install git -y
-sudo apt install curl -y
-sudo apt install snapd -y
+echo -e "\e[34;1mAtualizando o Sistema...\e[0m" && sleep 2
+sudo apt update && sudo apt upgrade -y
 
-echo -e "\e[34;1mBaixando e Instalando o Zsh e Oh My Zsh...\e[0m"
+if ! command -v curl &> /dev/null; then
+    echo -e "\e[34;1mInstalando o Curl...\e[0m"  && sleep 2
+    sudo apt install curl -y
+    if [ $? -ne 0 ]; then
+        echo -e "\e[31mErro ao instalar o Curl. Abortando...\e[0m"
+        exit 1
+    fi
+    else
+        echo -e "\e[33mO Curl já está instalado..\e[0m"  && sleep 1
+fi
+
+if ! command -v git &> /dev/null; then
+    echo -e "\e[34;1mInstalando Git...\e[0m"  && sleep 2
+    sudo apt install git -y
+    if [ $? -ne 0 ]; then
+        echo -e "\e[31mErro ao instalar o Curl. Abortando...\e[0m"
+        exit 1
+    fi
+    else
+        echo -e "\e[33mO Curl já está instalado..\e[0m"  && sleep 1
+fi
+
+echo -e "\e[34;1mInstalando ZSH...\e[0m"  && sleep 2
 sudo apt install zsh -y
+
+echo -e "\e[34;1mInstalando Oh My Zsh...\e[0m"  && sleep 2
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "0" | zsh-newuser-install
 echo "y" | chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo -e "\e[34;1mBaixando e Instalando o Plugins do Oh My Zsh...\e[0m"
+echo -e "\e[34;1mInstalando o Plugins do Oh My Zsh...\e[0m"  && sleep 2
 git clone https://github.com/z-shell/F-Sy-H ~/path/to/f-sy-h
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
@@ -23,20 +43,20 @@ echo 'plugins=(git zsh-autosuggestions)' >> ~/.zshrc
 echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc
 echo 'source ~/path/to/f-sy-h/F-Sy-H.plugin.zsh' >> ~/.zshrc
 
-echo -e "\e[34;1mBaixando e Instalando o Java...\e[0m"
+echo -e "\e[34;1mInstalando o Java...\e[0m" && sleep 2
 sudo apt install openjdk-21-jdk-headless -y
 
-echo -e "\e[34;1mBaixando e Instalando VsCode...\e[0m"
+echo -e "\e[34;1mInstalando VsCode...\e[0m" && sleep 2
 sudo snap install --classic code
 
-echo -e "\e[34;1mBaixando e Instalando NVM e Node...\e[0m"
+echo -e "\e[34;1mInstalando NVM e Node...\e[0m" && sleep 2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 
-echo -e "\e[34;1mBaixando e Instalando docker...\e[0m"
+echo -e "\e[34;1mBaixando e Instalando docker...\e[0m" && sleep 2
 sudo apt install docker.io docker-compose -y
 sudo systemctl enable --now docker docker.socket containerd
 
-echo -e "\e[32;1mFinalizado Instalações!!!\e[0m"
+echo -e "\e[32;1mFinalizado Instalações!!!\e[0m" && sleep 3
